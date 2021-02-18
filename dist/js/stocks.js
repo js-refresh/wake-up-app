68726688135df0190c8b9b6e5131ab9a4c814cde
Jake Luecke <jrluecke95@gmail.com>
2021-02-18 16:01:42 -0500

    built out carousel funciton for different graphs of each stock that we want to include

--- dist/js/stocks.js
+++ dist/js/stocks.js
@@ -1,24 +1,23 @@
 let stocksSection = $('#stocks');
 
-$.get("https://api.twelvedata.com/time_series?symbol=AAPL,MSFT&interval=1min&apikey=94052ab7a65c496786b0a1a7eaca0d03")
+$.get("https://api.twelvedata.com/time_series?symbol=AAPL,MSFT&interval=1week&apikey=94052ab7a65c496786b0a1a7eaca0d03")
 .then((data) => {
     let applePriceData = data.AAPL.values;
     let applePrices = [];
-    for (let i = 0; i < applePriceData.length; i++) {
+    for (let i = 25; i < applePriceData.length; i++) {
         applePrices.push(parseInt(applePriceData[i].open))
     }
-    console.log(applePrices)
+    console.log('apple', applePrices)
     let msftPriceData = data.MSFT.values;
     let msftPrices = [];
-    for (let i = 0; i < msftPriceData.length; i++) {
+    for (let i = 25; i < msftPriceData.length; i++) {
         msftPrices.push(parseInt(msftPriceData[i].open))
     }
 
-    let stockChart = document.getElementById('myChart');
-    var myChart = new Chart(stockChart, {
+    let msftChart = document.getElementById('msftChart');
+    let msftGraph = new Chart(msftChart, {
         type: 'line',
         data: {
-            labels: 'Stock Prices',
             datasets: [{
                 label: 'Microsoft',
                 data: msftPrices,
@@ -28,68 +27,6 @@ $.get("https://api.twelvedata.com/time_series?symbol=AAPL,MSFT&interval=1min&api
                     'rgba(0, 140, 255, 1)',
                     'rgba(0, 140, 255, 1)',
                     'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)',
-                    'rgba(0, 140, 255, 1)'
-                ],
-                borderWidth: 1
-            },
-            {
-                label: 'Apple',
-                data: applePrices,
-                borderColor: [
-                    'rgba(15, 206, 27, 1)', 
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)', 
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)', 
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)', 
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)', 
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)', 
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
-                    'rgba(15, 206, 27, 1)',
                 ],
                 borderWidth: 1
             }
@@ -106,5 +43,32 @@ $.get("https://api.twelvedata.com/time_series?symbol=AAPL,MSFT&interval=1min&api
         }
     });
 
+    let appleChart = document.getElementById('appleChart');
+    let appleGraph = new Chart(appleChart, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: ['Apple'],
+                data: applePrices,
+                borderColor: [
+                    'rgba(15, 206, 27, 1)', 
+                    'rgba(15, 206, 27, 1)',
+                    'rgba(15, 206, 27, 1)',
+                    'rgba(15, 206, 27, 1)',
+                    'rgba(15, 206, 27, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            }
+        }
+    });
 })
 
